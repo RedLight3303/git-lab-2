9e13c1cbc506980dd838e53bd69441af2a913588
RedLight3303 <jonathanvu.2023@gmail.com>
2024-01-24 19:45:25 -0500

    program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jonathan Vu
+ *        Date: 01/24/2024
+ * Description: New Project
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!" << endl;
+    /*add code*/
+    return 0;
+}// main
